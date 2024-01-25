4f357ae68051107362b518a54884fe18b045c531
Axhu99 <xhuliodreni@icloud.com>
2024-01-25 18:59:09 +0100

    add v-for for messages

--- js/script.js
+++ js/script.js
@@ -3,13 +3,16 @@ const {createApp} = Vue;
 
 const app = createApp({
     data() {
-        return{...data}
+        return{
+            ...data,
+        
+        }
     },
     methods:{
         getAvatarUrl({avatar}){
             return `img/avatar${avatar}.jpg`
         }
-    }
+    },
 })
 
 app.mount('#root');
