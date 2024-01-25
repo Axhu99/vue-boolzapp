f8046f2a1469eb4ede313cd992d8483ce057a85b
Axhu99 <xhuliodreni@icloud.com>
2024-01-25 19:46:28 +0100

    get chat by id

--- js/script.js
+++ js/script.js
@@ -1,18 +1,29 @@
 
 const {createApp} = Vue;
 
+const {user,contacts} = data;
+
 const app = createApp({
     data() {
         return{
-            ...data,
-        
+            user,
+            contacts,
+            contact: {},
         }
     },
     methods:{
         getAvatarUrl({avatar}){
             return `img/avatar${avatar}.jpg`
+        },
+        getContactById(id){
+
+            this.contact = this.contacts.find(contact => contact.id === id)
+
+            return this.contact
+            
         }
     },
+
 })
 
 app.mount('#root');
