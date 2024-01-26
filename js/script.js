c3fe70d4c02d48cab19c470a223b61e793a1e88b
Axhu99 <xhuliodreni@icloud.com>
2024-01-26 14:07:50 +0100

    add message

--- js/script.js
+++ js/script.js
@@ -8,20 +8,39 @@ const app = createApp({
         return{
             user,
             contacts,
-            contact: {},
+            contact:{},
+            newMessageText: '',
         }
     },
     methods:{
+        //restituicsce una scriga con dentro l' AVATAR delle elemento dato 
         getAvatarUrl({avatar}){
             return `img/avatar${avatar}.jpg`
         },
+
+        //metodo che mi restituisce il contatto con quel ID
         getContactById(id){
-
             this.contact = this.contacts.find(contact => contact.id === id)
+            return this.contact            
+        },
 
-            return this.contact
-            
-        }
+        addMessage(messageStatus,messageText){
+            const newMessage = {
+                id: 1,
+                date: '10/01/2020 15:30:55',
+                text: messageText,
+                status: messageStatus
+            }
+            this.contact.messages.push(newMessage);
+        },
+
+        addMessages(){
+            this.addMessage('sent',this.newMessageText);
+            setTimeout(() =>{
+                this.addMessage('received','ok')        
+            },1000)
+            this.newMessageText = '';
+        },
     },
 
 })
